f763e365576f33812a5c7f9c33bc285e432fdb94
Rohan pal <rohanpalportugal@gmail.com>
2025-08-31 01:19:45 +0100

    Update yt_video_Downloader.py

--- yt_video_Downloader.py
+++ yt_video_Downloader.py
@@ -1,10 +1,11 @@
 from pytube import YouTube
 
 try:
-    link = "https://www.youtube.com/watch?v=QmcJVZkdiTk"
+    link = "https://www.youtube.com/watch?v=QmcJVZkdiTk" #here paste the yt video link you want to download
     yt = YouTube(link)
     video = yt.streams.get_highest_resolution()
-    video.download("C:/Users/rohan/Downloads")
-    print("Download complete ✅")
+    video.download("C:/Users/rohan/Downloads") # here paste the adress of your folder where you want to get the video
+    print("Download complete")
 except Exception as e:
+
     print("Error:",e)
